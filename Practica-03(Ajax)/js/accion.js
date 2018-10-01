487f4ae2cd6a14f320b0c401603249dd86506eee
Alexsmg21 <33923446+Alexsmg21@users.noreply.github.com>
2018-10-01 23:22:50 +0200

    Practica Ajax
    Practica Ajax

---
+++ Practica-03(Ajax)/js/accion.js
@@ -0,0 +1,32 @@
+$(function () {
+    $("#button").on("click", function () {
+        $(this).css({
+            display:"none"
+        });
+
+
+        var resultado = $.ajax({
+            url: 'https://randomuser.me/api/',
+            dataType: 'json',
+            data: {
+                results: 50
+            }
+        });
+        resultado.done(function (data) {
+            for (var persona of data["results"]) {
+                $("#personas_aleatorias").append("<p>" + "<img src='" + persona["picture"]["large"] + "'>"+persona["name"]
+                        ["first"]+""+persona["name"]["last"]+"<br>"+persona["email"]+"<br>"+persona["location"]["street"]
+                    +""+persona["location"]["postcode"]+" "+[","]+" "+persona["location"]["city"]+""+[","]+""+persona["location"]["state"]+" </p>")
+
+            }
+            $("p").on("click",function () {
+                $(this).remove();
+
+
+            })
+
+        })
+    })
+
+
+});
